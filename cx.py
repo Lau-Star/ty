6e7df441828414798ce7b15b6dd9cf00612c2910
Lau-Star <65005057+Lau-Star@users.noreply.github.com>
2020-05-08 13:48:40 +0800

    Update cx.py

--- cx.py
+++ cx.py
@@ -2,6 +2,6 @@
 import requests
 token = 'c04f8d66e96046ff8d82e01c22ddb526' #在push+网站中可以找到
 title= '上课提醒！！' #改成你要的标题内容
-content ='课程名称：数据库原理与实践 \n时间：8点-12点\n地点：钉钉平台' #改成你要的正文内容
+content ='课程名称：矩阵与线性方程组 \n时间：8点-9点40分\n地点：腾讯课堂' #改成你要的正文内容
 url = 'http://pushplus.hxtrip.com/customer/push/send?token='+token+'&title='+title+'&content='+content
 requests.get(url)
