a4590a5718812fb24cbd47fed18f3f399bce6e91
MrliuXing520 <53454294+MrliuXing520@users.noreply.github.com>
2020-05-08 11:07:39 +0800

    Update cx.py

--- cx.py
+++ cx.py
@@ -5,5 +5,5 @@ key = "SCU96822Teacece914c94643dea76cf03bf3750755eb399b023adf" # your-key
 url = "https://sc.ftqq.com/%s.send"%(key)
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'}
 
-payload = {'text': 'Server酱提醒', 'desp': 'Python用Server酱推送微信模板消息'}
+payload = {'123': '上课提醒！！', 'desp': 'Python用Server酱推送微信模板消息'}
 requests.post(url, params=payload, headers=headers)
