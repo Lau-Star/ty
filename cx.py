6dc7725e2a2d4262fbe7b2be925caacd7f0c8be6
MrliuXing520 <53454294+MrliuXing520@users.noreply.github.com>
2020-05-08 12:10:32 +0800

    Update cx.py

--- cx.py
+++ cx.py
@@ -3,7 +3,7 @@ import requests
 import json
 token = 'c04f8d66e96046ff8d82e01c22ddb526' #在push+网站中可以找到
 title= '上课提醒！！' #改成你要的标题内容
-content ='课程名称：软件技术 时间：9点' #改成你要的正文内容
+content =' 课程名称：软件技术\n 时间：9点' #改成你要的正文内容
 url = 'http://pushplus.hxtrip.com/customer/push/send'
 data = {
     "token":token,
